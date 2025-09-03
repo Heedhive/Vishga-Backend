0773e470c753d95c26ac93e658217a8c1a80db58
muthuram05 <mr032495@gmail.com>
2025-09-03 22:01:45 +0530

    added requirement

--- extensions.py
+++ extensions.py
@@ -5,4 +5,4 @@ db = SQLAlchemy()
 
 # Use Render PostgreSQL (set in environment variable)
 DATABASE_URL = os.environ.get("DATABASE_URL") or \
-    "postgresql://db_rkrd_user:PzXukWQG6RGhyV9AwmjJfjpVdL9S44cg@dpg-d2km82bipnbc73f9gcg0-a.oregon-postgres.render.com/db_rkrd"
+    "postgresql://vishaga_db_user:sfqH9CKSp9GqZ8AT2S45kfSBKxNp86kO@dpg-d2s5pemmcj7s73ft3mb0-a.oregon-postgres.render.com/vishaga_db"
